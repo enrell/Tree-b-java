5046fe2222f88b0347552db83090f99861b85128
Ênrell <enrellsa10@gmail.com>
2022-12-19 22:09:34 -0300

    q01 respondida

--- q01/BinarySearchTree.java
+++ q01/BinarySearchTree.java
@@ -1,53 +1,50 @@
-class BinarySearchTree {
+import java.util.ArrayList;
+
+public class BinarySearchTree {
   
   class Node {
     int key;
-    String data;
+    ArrayList<String> data;
 
     Node right;
     Node left;
 
-    Node(int key, String value) {
+    Node(int key, ArrayList<String> value) {
       this.key = key;
       this.data = value;
     }
   }
+
   Node root;
+
   public BinarySearchTree() {
     this.root = null;
   }
 
-  private Node insert(Node node, int key, String value) {
+  private Node insert(Node node, int key, ArrayList<String> value) {
     if (node == null) {
       return new Node(key, value);
-    }
-    if (key < node.key) {
+    } else if (key < node.key) {
       node.left = insert(node.left, key, value);
     } else if (key > node.key) {
       node.right = insert(node.right, key, value);
-    }
-    return node;
-  }
-  
-  public void insert(String value) {
-    int hash = hash(value);
-    root = insert(root, hash, value);
+    } return node;
   }
 
-  public boolean sch(Node node, String value) {
+  public void insert(Node node, ArrayList<String> value) {
+    root = insert(node, hash(value), value);
+  }
+
+  public boolean search(Node node, String value) {
     if (node == null) {
       return false;
-    }
-    if (hash(value) < node.key) {
-      return sch(node.left, value);
-    } else if (hash(value) > node.key) {
-      return sch(node.right, value);
-    } else {
+    } else if (node.data.contains(value)) {
       return true;
     }
-  }
+    return search(node.left, value) || search(node.right, value);
+  }  
 
-  public Node remove(Node node, String value) {
+  public Node remove(Node node, ArrayList<String> value) {
     int key = hash(value);
     if (node == null) {
       return null;
@@ -79,6 +76,20 @@ class BinarySearchTree {
     }
     return node;
   }
+
+  public void removeValue(Node node, String value) {
+    if (node == null) {
+      return;
+    }
+    if (node.data.contains(value)) {
+      node.data.remove(value);
+      if (node.data.isEmpty()) {
+        node = remove(node, node.data);
+      }
+    }
+    removeValue(node.left, value);
+    removeValue(node.right, value);
+  }
   
   private Node findInorderSuccessor(Node node) {
     while (node.left != null) {
@@ -91,26 +102,18 @@ class BinarySearchTree {
     if (node == null) {
         return;
     }
-    if (node.data.equals(oldValue)) {
-        node.data = newValue;
-        return;
+    if (node.data.contains(oldValue)) {
+        int index = node.data.indexOf(oldValue);
+        node.data.set(index, newValue);
     }
     edit(node.left, oldValue, newValue);
     edit(node.right, oldValue, newValue);
-}
+  }  
 
   // Função de hash para gerar um valor de ordenação para uma string
-  private int hash(String value) {
-    // Converte a string em um array de bytes
-    byte[] bytes = value.getBytes();
-    // Inicializa o valor de hash como zero
-    int hash = 0;
-    // Itera sobre os bytes da string e atualiza o valor de hash
-    for (byte b : bytes) {
-      hash = 31 * hash + b;
-    }
-    return hash;
-  }
+  private int hash(ArrayList<String> str) {
+    return str.hashCode();
+}
 
   // Print the tree
   public void print(Node node) {
@@ -120,6 +123,36 @@ class BinarySearchTree {
     print(node.left);
     System.out.println(node.data);
     print(node.right);
+  }
+  public static void main(String[] args) {
+    ArrayList<String> name = new ArrayList<String>();
+    name.add("Meu nome");
+    ArrayList<String> phone = new ArrayList<String>();
+    phone.add("121234");
+    phone.add("763221");
+    ArrayList<String> email = new ArrayList<String>();
+    email.add("email1@email");
+    email.add("email2@email");
+    ArrayList<String> address = new ArrayList<String>();
+    address.add("Rua 1");
+    address.add("Rua 2");
+    ArrayList<String> social = new ArrayList<String>();
+    social.add("Instagram");
+    social.add("Facebook");
+
+    BinarySearchTree b = new BinarySearchTree();
+    b.insert(b.root, name);
+    b.insert(b.root, phone);
+    b.insert(b.root, email);
+    b.insert(b.root, address);
+    b.insert(b.root, social);
+    b.print(b.root);
+    System.out.println(" ");
+    b.edit(b.root, "Facebook", "Snap");
+    b.print(b.root);
+    System.out.println(" ");
+    b.removeValue(b.root, "Snap");
+    b.print(b.root);
   }
 
 }
